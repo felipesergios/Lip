aad9752c567264e99a1df541f6f4e294d3b29ba3
Felipe Sergio <39057272+felipesergios@users.noreply.github.com>
2019-10-09 09:55:01 -0300

    Update MATRIX.cpp
    Matrix Trasposta

--- MATRIX.cpp
+++ MATRIX.cpp
@@ -19,14 +19,27 @@ for(i=0;i<nl;i++){
     }
 
 }
+void Transposta(int n[][MAX],int mat_R[MAX][MAX],int nl,int nc,int& x,int& y){
+int i,j;
+for(i=0;i<nl;i++){
+    for(j=0;j<nc;j++){
+        mat_R[j][i]=n[i][j];
+        }
+    }
+x=nc;
+y=nl;
+}
 
 int main() {
-  int nl,nc;
-  int n[MAX][MAX];
+  int nl,nc,x,y;
+  int n[MAX][MAX]; int mat_R[MAX][MAX];
   cout<<"Ordem da Matriz N:";
   cin>>nl;
   cin>>nc;
   Preencher_M(n,nl,nc);
-  imprimir_M(n,nl,nc);
+  //imprimir_M(n,nl,nc);
+  Transposta(n,mat_R,nl,nc,x,y);
+  //cout<<"####"<<endl;
+  imprimir_M(mat_R,x,y);
   return 0;
 }
