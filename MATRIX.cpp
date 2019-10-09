ec659712d38bcab67a6740c89124756e8cdce5c8
Felipe Sergio <39057272+felipesergios@users.noreply.github.com>
2019-10-09 09:22:01 -0300

    Create MATRIX.cpp

---
+++ MATRIX.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+const int MAX=100;
+void Preencher_M(int n[][MAX],int nl,int nc){
+int i,j;
+for(i=0;i<nl;i++){
+    for(j=0;j<nc;j++){
+        cin>>n[i][j];
+        }
+    }
+}
+void imprimir_M(int n[][MAX],int nl,int nc){
+int i,j;
+for(i=0;i<nl;i++){
+    for(j=0;j<nc;j++){
+        cout<<n[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+int main() {
+  int nl,nc;
+  int n[MAX][MAX];
+  cout<<"Ordem da Matriz N:";
+  cin>>nl;
+  cin>>nc;
+  Preencher_M(n,nl,nc);
+  imprimir_M(n,nl,nc);
+  return 0;
+}
