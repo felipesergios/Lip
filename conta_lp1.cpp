3a18f2d85e6a44e645b4224b9de553257399c3a1
Felipe Sergio <39057272+felipesergios@users.noreply.github.com>
2022-04-01 21:13:48 -0300

    Change , using vector
    Updated with correct results

--- conta_lp1.cpp
+++ conta_lp1.cpp
@@ -21,34 +21,43 @@ int main() {
     int indice5=0;
     int valor_total=0;
     float porcent = 0;
+    int Intervalos[]={0,0,0,0,0};
     while(cin>>ws>>x){
         
         if(x>=0 and x<25){
-            indice++;
+            Intervalos[0]++;
         }
         if(x>=25 and x<50){
-            indice2++;
+            Intervalos[1]++;
         }
         if(x>=50 and x<75){
-            indice3++;
+            Intervalos[2]++;
         }
         if(x>=75 and x<100){
-            indice4++;
+            Intervalos[3]++;
         }
         
         if(x<0 or x>100){
-            indice5++;
+            Intervalos[4]++;
         }
+        valor_total++;
         
     }
-    cout<<"Fim do programa I: "<<indice<<endl;
-    cout<<"Fim do programa II : "<<indice2<<endl;
-    cout<<"Fim do programa III :"<<indice3<<endl;
-    cout<<"Fim do programa IV : "<<indice4<<endl;
-    cout<<"Fora de intervalo : "<<indice5<<endl;
-    valor_total=indice+indice2+indice3+indice4+indice5;
-    cout<<"Valor de todos os elementos "<<valor_total;
-    porcent=((indice*1.0)/valor_total)*100;
-    cout<<setprecision(4)<<"Porcentagem do valor I: "<<porcent;
+    //cout<<"Fim do programa I: "<<indice<<endl;
+    //cout<<"Fim do programa II : "<<indice2<<endl;
+    //cout<<"Fim do programa III :"<<indice3<<endl;
+   // cout<<"Fim do programa IV : "<<indice4<<endl;
+   // cout<<"Fora de intervalo : "<<indice5<<endl;
+   // valor_total=indice+indice2+indice3+indice4+indice5;
+  //  cout<<"Valor de todos os elementos "<<valor_total;
+   // porcent=((indice*1.0)/valor_total)*100;
+  //  cout<<setprecision(4)<<"Porcentagem do valor I: "<<porcent;
+  int arrSize = sizeof(Intervalos)/sizeof(Intervalos[0]);
+  for(int i=0;i<arrSize;i++){
+      cout<<"Intervalo"<<i<<" - "<<Intervalos[i]<<endl;
+      porcent=((Intervalos[i]*1.0)/valor_total)*100;
+      cout<<"Porcentagem "<<porcent<<endl;
+  }
+  cout<<"Numeros digitados "<<valor_total<<endl;
     return 0;    
 }
