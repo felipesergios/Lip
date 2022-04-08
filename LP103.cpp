6ca56bd47b419eb369c12d2417d1c4b22f8be37a
Felipe Sergio <39057272+felipesergios@users.noreply.github.com>
2022-04-08 20:23:20 -0300

    Change logic

--- LP103.cpp
+++ LP103.cpp
@@ -1,10 +1,13 @@
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int m , n , aux , result = 0;
+    int m , n , aux = 0;
+    int  reverse = 0;
+    int reverse_result = 0;
     cin>>m;
     cin>>n;
     
@@ -12,27 +15,27 @@ int main()
     if(n>0){
         aux=m;
         for(int i=0;i<n;i++){
-            cout<<aux;
-            result+=aux;
+            reverse_result+=aux;
             aux++;
             
         }
     }
     
-    if(n<0){
-        aux=m;
-        for(int i=m;i>=n;i--){
-            cout<<i;
-            result+=aux;
-            aux--;
-            
+   if(n<0){
+        aux=n*(-1);
+        reverse=m;
+        for(int i=0;i<aux;i++){
+          reverse_result+=reverse;
+            reverse-=1;
         }
     }
     
     
     if(n==0){
         cout<<m;
+        return 0;
     }
-    cout<<"A soma é :"<<result;
+    cout<<reverse_result;
+
     return 0;
 }
