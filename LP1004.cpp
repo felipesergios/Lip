402a6c465d26a3c397f62b0208af735781b9a8de
Felipe Sergio <39057272+felipesergios@users.noreply.github.com>
2022-04-08 20:57:47 -0300

    Update and rename LP1004 to LP1004.cpp

--- LP1004.cpp
+++ LP1004.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main() {
-    int
     int n = 0;
     int t1 = 0;
     int t2 = 1;
@@ -14,13 +13,13 @@ int main() {
         int t3=t1+t2;
         if(t3 < n){
     cout<<" -> "<<t3;
+ 
     t1=t2;
     t2=t3;
     cont +=1;
         }else{
             return 0;
         }
-        
         
     }
     return 0;
